c6a7f074699a3540fc7a8ee1dd85736135cfa8d5
rwlockbg <evgeni@botanixlabs.xyz>
2025-11-21 12:54:41 +0200

    added pegin-discovery, fixes to sched, tests and Makefile

---
+++ bin/pegin-recovery/client/src/lib.rs
@@ -0,0 +1,5 @@
+mod pegin_recovery;
+
+pub use pegin_recovery::{
+    pegin_recovery_service_client::PeginRecoveryServiceClient, *,
+};
---
+++ bin/pegin-recovery/src/rpc/mod.rs
@@ -0,0 +1,11 @@
+// Generated protobuf code
+#![allow(clippy::all)]
+#![allow(warnings)]
+
+pub mod pegin_recovery {
+    include!("pegin_recovery.rs");
+
+    pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("pegin_recovery.bin");
+}
+
+pub use pegin_recovery::*;
