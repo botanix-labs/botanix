fc0dd792db16f08b31ebc8399cd2d33513294c32
Tobi Akinrinade <71441407+thobbyAk@users.noreply.github.com>
2025-12-11 01:26:50 +0000

    Fix rpc get block by number (#50)
    * fix(rpc): added test method on getBlockByNumber
    
    * fix(rpc): added custom rpc method eth_richBlockByNumber
    
    * fix(rpc): remove unused traitbounds and refactor error messages

--- bin/botanix-reth/src/services/rpc/botanixrpc_ext.rs
+++ bin/botanix-reth/src/services/rpc/botanixrpc_ext.rs
@@ -1,12 +1,14 @@
 use alloy_primitives::{Address, Bytes, U256};
+use alloy_rpc_types_eth::{BlockNumberOrTag};
 use botanix_authority_edh::header_ext::HeaderExt;
 use botanix_rpc_client::botanix::EthBotanixApi;
 use botanix_rpc_config::{botanix_config::Botanix, result::ToRpcResult};
-use botanix_rpc_types::types::GatewayAddress;
+use botanix_rpc_types::types::{GatewayAddress, RichBlock};
 use jsonrpsee_core::RpcResult;
 // Reth block related imports
 use reth_ethereum::provider::BlockReaderIdExt;
 use reth_provider::HeaderProvider;
+use reth_rpc_eth_api::helpers::EthBlocks;
 
 // Rpc related imports
 use jsonrpsee::proc_macros::rpc;
@@ -42,21 +44,34 @@ pub trait BotanixRpcExtApi {
     /// Method to get the btc fee rate
     #[method(name = "getBtcFeeRate")]
     async fn get_btc_fee_rate(&self) -> RpcResult<Option<U256>>;
+
+    /// Method to get a rich block by number with optional extra data header
+    #[method(name = "richBlockByNumber")]
+    async fn rich_block_by_number(
+        &self,
+        number: BlockNumberOrTag,
+        full: bool,
+        include_extra_data_header: Option<bool>,
+    ) -> RpcResult<Option<RichBlock>>;
+
 }
 
 /// The type that implements `botanixrpcExt` rpc namespace trait
-pub struct BotanixRpcExt<Provider> {
+pub struct BotanixRpcExt<Provider, EthApi> {
     /// The Ethereum provider used to read blocks.
     pub provider: Provider,
     /// Botanix client and configuration.
     pub botanix: Botanix,
+    /// The Ethereum API for calling standard eth methods
+    pub eth_api: EthApi,
 }
 
 #[async_trait::async_trait]
-impl<Provider> BotanixRpcExtApiServer for BotanixRpcExt<Provider>
+impl<Provider, EthApi> BotanixRpcExtApiServer for BotanixRpcExt<Provider, EthApi>
 where
     Provider: BlockReaderIdExt<Block = BotanixBlock> + Clone + 'static,
     <Provider as HeaderProvider>::Header: HeaderExt,
+    EthApi: EthBlocks + Send + Sync + 'static,
 {
     async fn aggregate_public_key(&self) -> RpcResult<PublicKey> {
         self.botanix
@@ -96,12 +111,27 @@ where
     async fn get_btc_fee_rate(&self) -> RpcResult<Option<U256>> {
         self.botanix.get_btc_fee_rate().await.map(Some).to_rpc_result()
     }
+
+    /// Handler for: `eth_richBlockByNumber`
+    async fn rich_block_by_number(
+        &self,
+        number: BlockNumberOrTag,
+        full: bool,
+        include_extra_data_header: Option<bool>,
+    ) -> RpcResult<Option<RichBlock>> {
+        self.botanix
+            .rich_block_by_number(&self.eth_api, number, full, include_extra_data_header)
+            .await
+            .to_rpc_result()
+    }
+
 }
 
-impl<Provider> EthBotanixApi for BotanixRpcExt<Provider>
+impl<Provider, Eth> EthBotanixApi for BotanixRpcExt<Provider, Eth>
 where
     Provider: BlockReaderIdExt<Block = BotanixBlock> + Clone + 'static,
     <Provider as HeaderProvider>::Header: HeaderExt,
+    Eth: Send + Sync + 'static,
 {
     fn provider(&self) -> impl BlockReaderIdExt {
         self.provider.clone()
