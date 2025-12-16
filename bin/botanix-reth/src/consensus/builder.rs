061dd2136282b68fd460831663b53a2a32f271dd
rwlockbg <evgeni@botanixlabs.xyz>
2025-12-17 01:34:53 +0200

    removed spawned task

--- bin/botanix-reth/src/consensus/builder.rs
+++ bin/botanix-reth/src/consensus/builder.rs
@@ -482,57 +482,50 @@ where
             );
         }
 
-
-        let mut btc_server_client_clone = btc_server_client.clone();
-        let storage_clone = storage.clone();
-        task_executor.spawn_critical(
-            "persist to storage all multisig ids",
-            Box::pin(async move {
-                if let Some(btc_server) = btc_server_client_clone.as_mut() {
-                    let multisig_ids = match btc_server.list_multisigs(Empty {}).await {
-                        Ok(multisig_ids) => {
-                            info!(target: "reth::authority", "Found {} multisig ids", multisig_ids.ids.len());
-                            multisig_ids
-                        }
-                        Err(e) => {
-                            tracing::error!(target: "reth::authority", "Error getting multisig ids: {}", e);
-                            return;
-                        }
-                    }
-                    .ids;
-                    
-                    let mut aggregated_pub_keys = vec![];
-                    for multisig_id in multisig_ids {
-                        match btc_server.get_public_key(GetPublicKeyRequest {
-                            multisig_id,
-                        })
-                        .await {
-                            Ok(resp) => {
-                                if let Ok(pk) = secp256k1::PublicKey::from_str(&resp.publickey) {
-                                    let multisig_id: MultisigId = multisig_id.into();
-                                    aggregated_pub_keys.push((multisig_id, pk));
-                                } else {
-                                    tracing::error!(target: "reth::authority", "Error parsing public key for multisig id: {}", multisig_id);
-                                    continue;
-                                }
-                            }
-                            Err(e) => {
-                                tracing::error!(target: "reth::authority", "Error retrieving public key for multisig id: {}, e = {}", multisig_id, e);
-                                continue;
-                            }
+        // load all multisig ids and aggregated public keys into the storage
+        if let Some(btc_server) = btc_server_client.as_mut() {
+            let multisig_ids = match btc_server.list_multisigs(Empty {}).await {
+                Ok(multisig_ids) => {
+                    info!(target: "reth::authority", "Found {} multisig ids", multisig_ids.ids.len());
+                    multisig_ids
+                }
+                Err(e) => {
+                    tracing::error!(target: "reth::authority", "Error getting multisig ids: {}", e);
+                    panic!("Error getting multisig ids: {}", e);
+                }
+            }
+            .ids;
+            
+            let mut aggregated_pub_keys = vec![];
+            for multisig_id in multisig_ids {
+                match btc_server.get_public_key(GetPublicKeyRequest {
+                    multisig_id,
+                })
+                .await {
+                    Ok(resp) => {
+                        if let Ok(pk) = secp256k1::PublicKey::from_str(&resp.publickey) {
+                            let multisig_id: MultisigId = multisig_id.into();
+                            aggregated_pub_keys.push((multisig_id, pk));
+                        } else {
+                            tracing::error!(target: "reth::authority", "Error parsing public key for multisig id: {}", multisig_id);
+                            panic!("Error parsing public key for multisig id: {}", multisig_id);
                         }
                     }
-
-                    if !aggregated_pub_keys.is_empty() {
-                        let mut storage = storage_clone.write().await;
-                        if let Some(storage_pub_keys) = storage.aggregate_public_key.as_mut() {
-                            storage_pub_keys.extend(aggregated_pub_keys);
-                        }
-                        drop(storage);
+                    Err(e) => {
+                        tracing::error!(target: "reth::authority", "Error retrieving public key for multisig id: {}, e = {}", multisig_id, e);
+                        panic!("Error retrieving public key for multisig id: {}, e = {}", multisig_id, e);
                     }
                 }
-            })
-        );
+            }
+
+            if !aggregated_pub_keys.is_empty() {
+                let mut storage = storage.write().await;
+                if let Some(storage_pub_keys) = storage.aggregate_public_key.as_mut() {
+                    storage_pub_keys.extend(aggregated_pub_keys);
+                }
+                drop(storage);
+            }
+        }
 
         (
             frost_task,
